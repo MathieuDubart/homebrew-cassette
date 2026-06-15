68d92196401a92f1e458255cd015e2f695324c78
Mathieu Dubart <mathieu.dubart@icloud.com>
2026-06-15 19:55:14 +0200

    fix: dynamic version tag in url

--- Casks/cassette.rb
+++ Casks/cassette.rb
@@ -2,7 +2,7 @@ cask "cassette" do
   version "1.8.0"
   sha256 "2d8cc77128d7d55fd716bb46b726c48f58ee1a8acd39e28e74f9d8357f3b9df5"
 
-  url "https://github.com/MathieuDubart/cassette/releases/download/v1.8.0/Cassette-1.8.0.dmg",
+  url "https://github.com/MathieuDubart/cassette/releases/download/v#{version}/Cassette-#{version}.dmg",
     verified: "github.com/MathieuDubart/cassette/"
   name "Cassette"
   desc "Subsonic music client"
